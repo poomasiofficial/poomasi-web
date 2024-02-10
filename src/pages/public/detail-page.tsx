cf671d11eb1ad04b12769421246fae4d815cd1ec
poomasiofficial <>
2024-02-10 08:50:43 +0000

    zz

--- src/pages/public/detail-page.tsx
+++ src/pages/public/detail-page.tsx
@@ -7,6 +7,15 @@ import { AccountResponse, RequestApi } from '@api'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Switch from '@mui/material/Switch'
+import InputLabel from '@mui/material/InputLabel'
+import NativeSelect from '@mui/material/NativeSelect'
+import FormControl from '@mui/material/FormControl'
+import Button from '@mui/material/Button'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+
 export function DetailPage() {
   useToastClear()
   const navigate = useNavigate()
@@ -45,8 +54,64 @@ export function DetailPage() {
 
           <div style={{ marginTop: '30px', fontWeight: 'bold', fontSize: '20px' }}>자기소개</div>
           <Description readOnly value={account?.description} />
+
+          <Seperator />
+
           <Body>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div style={{ display: 'flex' }}>
+                <div style={{ display: 'flex', alignItems: 'center', fontWeight: 'bold', fontSize: '20px' }}>질문하기</div>
+                <div
+                  style={{ fontSize: '16px', marginLeft: '3px', marginTop: '1px', display: 'flex', alignItems: 'center', color: 'var(--gray-color)' }}
+                >
+                  (0 / 500)
+                </div>
+              </div>
+              <FormControlLabel style={{ margin: '0' }} control={<Switch />} label="비밀질문" />
+            </div>
+
             <QuestionArea />
+
+            <div style={{ marginTop: '7px', display: 'flex', justifyContent: 'space-between' }}>
+              <div style={{ display: 'flex' }}>
+                <FormControl>
+                  <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                    개발 경력
+                  </InputLabel>
+                  <NativeSelect
+                    defaultValue={'대학생'}
+                    inputProps={{
+                      name: 'age',
+                      id: 'uncontrolled-native',
+                    }}
+                  >
+                    <option value={'대학생'}>대학생</option>
+                    <option value={'취준생'}>취준생</option>
+                    <option value={'1~3년차'}>1~3년차</option>
+                    <option value={'3년차 이상'}>3년차 이상</option>
+                  </NativeSelect>
+                </FormControl>
+
+                <RadioGroup row style={{ marginLeft: '7px' }}>
+                  <FormControlLabel value="전공" control={<Radio size="small" />} label="전공" />
+                  <FormControlLabel value="비전공" control={<Radio size="small" />} label="비전공" />
+                </RadioGroup>
+              </div>
+
+              <Button
+                variant="contained"
+                sx={{
+                  width: '60px',
+                  height: '40px',
+                  fontSize: '16px',
+                  fontWeight: 'bold',
+                  borderRadius: '10px',
+                  color: 'white',
+                }}
+              >
+                등록
+              </Button>
+            </div>
           </Body>
         </PageContent>
       </PageContainer>
@@ -140,6 +205,8 @@ const HeaderField = styled.div`
 `
 const HeaderJob = styled.div`
   font-size: 20px;
+  font-weight: bold;
+  color: var(--light-gray-color);
   @media (max-width: 520px) {
     font-size: 16px;
   }
@@ -154,17 +221,36 @@ const Description = styled(TextareaAutosize)`
   resize: none;
   font-size: 17px;
   padding: 0;
+
+  @media (max-width: 520px) {
+    font-size: 14px;
+  }
   /* background-color: green; */
+`
+const Seperator = styled.div`
+  height: 4px;
+  width: 100%;
+  border-top: 3px var(--light-gray-color) dashed;
+  margin-top: 30px;
 `
 
 const Body = styled.div`
-  margin-top: 30px;
+  margin-top: 20px;
   width: 100%;
-  background-color: red;
+  height: 300px;
+  /* background-color: greenyellow; */
 `
 
 const QuestionArea = styled.textarea`
+  margin-top: 5px;
   box-sizing: border-box;
   width: 100%;
-  background-color: green;
+  height: 60%;
+  border-radius: 10px;
+  resize: none;
+
+  @media (max-width: 520px) {
+    height: 40%;
+  }
+  /* background-color: green; */
 `
