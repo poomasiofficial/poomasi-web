c50b9ec08c8dac064bfd738f3b231f65a4571fab
poomasiofficial <>
2024-04-14 13:14:02 +0000

    fix : 배포 오류 수정

--- app/(page)/(public)/(landing)/page.tsx
+++ app/(page)/(public)/(landing)/page.tsx
@@ -1,32 +1,4 @@
 import { ReactNode } from "react";
 import { DescriptionSection, FooterSection, PeopleSection, TitleSection } from "@page";
 
-export const LandingPage = () => {
-  return (
-    <div className="w-full pt-20">
-      <div className="flex items-center justify-center w-full px-[5%]">
-        <div className="w-[1200px] mb-10">
-          <TitleSection />
-        </div>
-      </div>
-
-      <div className="flex items-center justify-center w-full px-[5%]">
-        <div className="w-[1200px] mb-10">
-          <DescriptionSection />
-        </div>
-      </div>
-
-      <div className="flex items-center justify-center w-full px-[5%]">
-        <div className="w-[1200px] mb-10">
-          <PeopleSection />
-        </div>
-      </div>
-
-      <div className="flex items-center justify-center w-full px-[5%]">
-        <div className="w-[1200px] mb-10">
-          <FooterSection />
-        </div>
-      </div>
-    </div>
-  );
-};
+export const LandingPage = () => {};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import { LandingPage } from "@page";
+import { DescriptionSection, FooterSection, LandingPage, PeopleSection, TitleSection } from "@page";
 
 export default function Home() {
   return (
     <div>
-      <LandingPage />
+      <div className="w-full pt-20">
+        <div className="flex items-center justify-center w-full px-[5%]">
+          <div className="w-[1200px] mb-10">
+            <TitleSection />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center w-full px-[5%]">
+          <div className="w-[1200px] mb-10">
+            <DescriptionSection />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center w-full px-[5%]">
+          <div className="w-[1200px] mb-10">
+            <PeopleSection />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center w-full px-[5%]">
+          <div className="w-[1200px] mb-10">
+            <FooterSection />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
