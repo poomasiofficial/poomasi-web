75c17d20958aa4e091b23e87ef396b6acded5ed3
poomasiofficial <>
2025-04-26 13:03:26 +0000

    feat: 폰트 SUIT 로 설정하도록 수정

--- src/styles/global-theme.ts
+++ src/styles/global-theme.ts
@@ -1,9 +1,7 @@
 import { createTheme } from '@mui/material/styles'
 
 export const globalTheme = createTheme({
-  typography: {
-    fontFamily: 'Pretendard',
-  },
+  typography: {},
   palette: {},
   components: {},
 })
